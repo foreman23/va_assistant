ecc0f1b0ea6cfc91d1c19a9eb17d1a23cfe09e19
foreman23 <foremanb@oregonstate.edu>
2023-09-30 23:59:45 -0700

    Implemented voice recording and recognition

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import speech_recognition as sr
 
 
-# # obtain audio from the microphone
-# r = sr.Recognizer()
-#
-# # Get microphones list
-# mic_arr = sr.Microphone.list_microphone_names()
-# # print(mic_arr[1])
-# mic = sr.Microphone(device_index=1)
-#
-# with mic as source:
-#     audio = r.listen(source)
-#
-# # recognize speech using Sphinx
-# try:
-#     print(r.recognize_sphinx(audio))
-# except sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
-
-
 def startApp(app):
     print("Welcome to the app")
 
@@ -30,6 +10,8 @@ def startApp(app):
         print("Please choose a command:")
         print("1: View current input device")
         print("2: Change input device")
+        print("3: Record voice")
+        print("4: Get last voice prompt")
         print("x: Exit application")
         command = input("::")
 
@@ -39,27 +21,45 @@ def startApp(app):
         if command == "2":
             app.setMic(None)
 
+        if command == "3":
+            app.recordPrompt()
+
+        if command == "4":
+            print(app.getPrompt())
+
         if command == "x":
             loop = False
-
-        # app.setMic(None)
 
 
 class ProgramLoop:
 
     def __init__(self):
-        self.mic_name = sr.Microphone.list_microphone_names()[0]
+        self.mic_name = sr.Microphone.list_microphone_names()[1]
         self.mic_obj = sr.Microphone()
+        self.prompt = None
 
-    # Returns the name of the current mic
     def getMicName(self):
+        """
+        Returns the NAME of the current mic
+        """
         return self.mic_name
 
     def getMicObj(self):
+        """
+        Returns the current mic OBJECT
+        """
         return self.mic_obj
 
-    # Sets the current mic from system list (optional index param)
+    def getPrompt(self):
+        """
+        Returns the current prompt
+        """
+        return self.prompt
+
     def setMic(self, index):
+        """
+        Sets the current mic from system list (optional index param)
+        """
         mic_arr = sr.Microphone.list_microphone_names()
         if index is not None:
             mic_obj = sr.Microphone(device_index=index)
@@ -74,6 +74,22 @@ class ProgramLoop:
             mic_obj = sr.Microphone(device_index=index)
             self.mic_name = mic_arr[index]
             self.mic_obj = mic_obj
+
+    def recordPrompt(self):
+        r = sr.Recognizer()
+
+        with self.mic_obj as source:
+            audio = r.listen(source)
+
+        # recognize speech using Sphinx
+        try:
+            speech = r.recognize_sphinx(audio)
+            print(speech)
+            self.prompt = speech
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
 
 
 if __name__ == "__main__":
